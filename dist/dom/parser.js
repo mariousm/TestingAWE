75b4428656c9e96f22c02bd1abbf89eb28444a3b
Mario <mus0007@alu.ubu.es>
2019-04-09 19:25:12 +0200

    Creación Expresión Regular
    He creado la expresión regular, para así poder identificar si un id va a ser dinámico o no.

--- dist/dom/parser.js
+++ dist/dom/parser.js
@@ -28,8 +28,18 @@ function parser(path) {
 // Entrada: lista que devuelve el parser
 function logicParser(path) {
     let list = [];
+    let regExpr = new RegExp('\{\{[aA-zZ>]+\}\}'); //Expresión regular para determinar si un id es dinámico o no
     list = parser(path);
     console.log(list);
+    for (let node of list) {
+        if (regExpr.test(node[1])) {
+            //Es que es un id dinámico
+            console.log("Es un elemento dinámico");
+        }
+        else {
+            console.log("Es un elemento estático");
+        }
+    }
     // TODO recorrer la lista y mirar si elemento es estático, llamar a un método para que escriba el código necesario
     // si es un elemento dinámico, abstraer {{ lo que sea }} y generar un objeto parametrizado en el fichero intermedio,
     // cuyo nombre el método sea el nombre del id sin el {{ lo que sea }}, cuyo parámetro sea un número, y dentro de ese
--- lib/dom/parser.ts
+++ lib/dom/parser.ts
@@ -5,9 +5,9 @@ var htmlparser = require("htmlparser2");
 // Convierte el HTML en un String sin espacios entre etiquetas
 function htmlToString(path: string): string {
 
-    let toReturn : string = ''
+    let toReturn : string = '';
 
-    toReturn = readFileSync(path, 'utf8').replace(/>\s+</g, "><")
+    toReturn = readFileSync(path, 'utf8').replace(/>\s+</g, "><");
 
     return toReturn.trim()
 }
@@ -15,18 +15,18 @@ function htmlToString(path: string): string {
 // Devuelve el tipo de etiqueta HTML que lee junto con su id
 function parser(path: string): Array<Array<string>> {
 
-    let list: Array<Array<string>> = []
-    let html: string = htmlToString(path)
+    let list: Array<Array<string>> = [];
+    let html: string = htmlToString(path);
 
     var parser = new htmlparser.Parser({
         onopentag: function(name: string, attribs: { id: string; }){
             if(attribs.id !== undefined && attribs.id !== ""){
-                list.push([name, attribs.id])
+                list.push([name, attribs.id]);
             }
         }
     }, {decodeEntities: true});
-    parser.write(html)
-    parser.end()
+    parser.write(html);
+    parser.end();
     
     return list
 }
@@ -35,12 +35,20 @@ function parser(path: string): Array<Array<string>> {
 // Entrada: lista que devuelve el parser
 export function logicParser(path: string) {
 
-    let list: Array<Array<string>> = []
+    let list: Array<Array<string>> = [];
+    let regExpr: RegExp = new RegExp('\{\{[aA-zZ>]+\}\}'); //Expresión regular para determinar si un id es dinámico o no
 
     list = parser(path)
-
     console.log(list)
 
+    for (let node of list) {
+        if (regExpr.test(node[1])){ 
+            //Es que es un id dinámico
+            console.log("Es un elemento dinámico");
+        } else {
+            console.log("Es un elemento estático");
+        }
+    }
     // TODO recorrer la lista y mirar si elemento es estático, llamar a un método para que escriba el código necesario
     // si es un elemento dinámico, abstraer {{ lo que sea }} y generar un objeto parametrizado en el fichero intermedio,
     // cuyo nombre el método sea el nombre del id sin el {{ lo que sea }}, cuyo parámetro sea un número, y dentro de ese
