02bfe2e47d61637281c7ec44bfd05e5caed26fd1
Mario <mus0007@alu.ubu.es>
2019-05-25 12:11:01 +0200

    Inicio Test Unitarios

---
+++ test/spec_parser.ts
@@ -0,0 +1,13 @@
+// IMPORTACIONES
+//import { assert } from "chai";
+let assert = require("chai").assert;
+let parser = require("../dist/parser/parser");
+
+describe("UnitTest PARSER", () => {
+
+    it.only("Prueba", () => {
+        const x = 0;
+        assert.equal(x, 1)
+    });
+
+});
