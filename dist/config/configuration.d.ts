c7c20d11e7ab7a0bc38f55b5b42e8cf0a11e2cba
Mario <mus0007@alu.ubu.es>
2019-05-26 12:39:05 +0200

    Unit Test para la configuración

--- dist/config/configuration.d.ts
+++ dist/config/configuration.d.ts
@@ -1,3 +1,10 @@
+export declare function jsonToString(path: string): string;
+export declare function getRootDirectory(): string;
+export declare function getNameFileConfig(): string;
+export declare function createJson(): void;
+export declare function getObjJson(): any;
+export declare function checkExistFile(): boolean;
+export declare function checkExistProjectProperty(): Array<boolean>;
 export declare function checkAweconfig(): boolean;
 export declare function getProjectPath(): Array<string>;
 export declare function getIgnore(): Array<string>;
