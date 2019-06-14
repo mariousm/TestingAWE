e519abee73bf1913a41c05b456bacb3fa0299443
Mario <mus0007@alu.ubu.es>
2019-06-14 18:22:47 +0200

    Eliminado Script y Style, y modificado los test unitarios
    - Script no tiene uso dentro de Angular/Electron.
    - Style no es un elemento con el que se pueda interacutar.

--- dist/libraryWIO/class/Script.d.ts
+++
@@ -1,4 +0,0 @@
-import { Root } from "./Root";
-export declare class Script extends Root {
-    constructor(id: string);
-}
--- dist/libraryWIO/class/Style.d.ts
+++
@@ -1,4 +0,0 @@
-import { Root } from "./Root";
-export declare class Style extends Root {
-    constructor(id: string);
-}
